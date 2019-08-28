f271018c09d8f8deb66212411018d4b21eaf664a
travisfranklin <travislf88@gmail.com>
2019-08-27 20:39:56 -0500

    adjust organization of main.js for clarity

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,33 @@
 // MODEL
-///////////////////
 
 // Create a prototype object for generating cats
 function Kitty(name) {
   this.name = name,
-  this.clickCount = 0,
-  this.clicker = function clicker() {
-    this.clickCount++
-    $(`.${this.name}clicked`).text(clickCount);
+  this.clickCount = 0
+};
+
+// VIEW
+
+// Generate HTML to display cats from playPen array.
+function renderCat() {
+  for (let cat of playPen) {
+    $(`
+      <li>
+          <div class="cat-unit">
+            <div class="cat cat_type18">
+              <div id="${cat.name}" class="cat__previewbox">
+                <p class="title">${cat.name}</p>
+                <img src="${cat.photo}" class="cat__preview" alt="photo of ${cat.name} the cat">
+                <p class="count"><span class="${cat.name}clicked">0</span> clicks</p>
+              </div>
+            </div>
+          </div>
+        </li>
+    `).appendTo('.catListContainer');
   };
 };
+
+// CONTROLLER
 
 // Define the cat names
 const cats = [
@@ -21,7 +39,10 @@ const cats = [
   'whiskers',
   'bulgaria',
   'mr_Rogers',
-  'dog'
+  'dog',
+  'bucko',
+  'marilyn',
+  'indy'
 ];
 
 // Define an array in which we can place our cats
@@ -37,34 +58,10 @@ for (let [index ,cat] of cats.entries()) {
   playPen.push(cat);
 };
 
-///////////////////////////////////////////////////////////////////////////////
+// Call function to render cats
+renderCat();
 
-// VIEW
-///////////////////
-
-// Generate HTML to display cats from playPen array.
-for (let cat of playPen) {
-  $(`
-    <li>
-        <div class="cat-unit">
-          <div class="cat cat_type18">
-            <div id="${cat.name}" class="cat__previewbox">
-              <p class="title">${cat.name}</p>
-              <img src="${cat.photo}" class="cat__preview" alt="photo of ${cat.name} the cat">
-              <p class="count"><span class="${cat.name}clicked">0</span> clicks</p>
-            </div>
-          </div>
-        </div>
-      </li>
-  `).appendTo('.catListContainer');
-};
-
-
-///////////////////////////////////////////////////////////////////////////////
-
-// CONTROLLER
-///////////////////
-
+// Add click event listener to cat images and increment count on each when clicked
 playPen.forEach(function(cat) {
   $(`#${cat.name}`).click(function() {
     cat.clickCount++;
