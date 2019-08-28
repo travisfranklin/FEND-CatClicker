c40f17b66aa1f9c7c05bb7965c9d6c4a4fe8c03d
travisfranklin <travislf88@gmail.com>
2019-08-28 11:08:42 -0500

    add cat selection list functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,18 +12,24 @@ function Kitty(name) {
 function renderCat() {
   for (let cat of playPen) {
     $(`
-      <li>
-          <div class="cat-unit">
-            <div class="cat cat_type18">
-              <div id="${cat.name}" class="cat__previewbox">
-                <p class="title">${cat.name}</p>
-                <img src="${cat.photo}" class="cat__preview" alt="photo of ${cat.name} the cat">
-                <p class="count"><span class="${cat.name}clicked">0</span> clicks</p>
-              </div>
-            </div>
-          </div>
-        </li>
-    `).appendTo('.catListContainer');
+      <li id="${cat.name}" class="cat hidden">
+        <div  class="cat__previewbox">
+          <div class="cat__overlay"></div>
+          <p class="title">${cat.name}</p>
+          <img src="${cat.photo}" class="cat__preview" alt="photo of ${cat.name} the cat">
+          <p class="count"><span class="${cat.name}clicked">0</span> clicks</p>
+        </div>
+      </li>
+    `).appendTo('.catList__Container');
+  };
+};
+
+// Generate HTML to display cat list links
+function renderCatList() {
+  for (let cat of playPen) {
+    $(`
+      <li id="${cat.name}__Link" class="catList__Link">${cat.name}</li>
+    `).appendTo('.catList__Links');
   };
 };
 
@@ -54,18 +60,38 @@ for (let [index ,cat] of cats.entries()) {
     this.name = cat;
   };
   cat = new Kitty(cat);
-  cat.photo = `https://loremflickr.com/200/200/kitten,sleeping/all/?lock=${Math.floor(Math.random() * Math.floor(100))}`;
+  cat.photo = `https://loremflickr.com/200/200/kitten,sleeping/all/?lock=${Math.floor(Math.random() * Math.floor(1000))}`;
   playPen.push(cat);
 };
 
 // Call function to render cats
+renderCatList();
 renderCat();
 
-// Add click event listener to cat images and increment count on each when clicked
+
+// Add click event listener to cat images
 playPen.forEach(function(cat) {
   $(`#${cat.name}`).click(function() {
+
+    // increment count when clicked
     cat.clickCount++;
     $(`.${cat.name}clicked`).text(cat.clickCount);
+  });
+});
+
+// Add click event listener to cat list links
+playPen.forEach(function(cat) {
+  $(`#${cat.name}__Link`).click(function() {
+
+    // toggle visibility on all other cats
+    playPen.forEach(function(cat) {
+      if(!$(`#${cat.name}`).hasClass('hidden')){
+        $(`#${cat.name}`).addClass('hidden');
+      };
+    });
+
+    // make the clicked cat visible
+    $(`#${cat.name}`).toggleClass('hidden');
   });
 });
 
