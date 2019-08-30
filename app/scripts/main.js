cfc4790873af5e096df61918b8bcecbb8eae49df
travisfranklin <travislf88@gmail.com>
2019-08-30 11:32:43 -0500

    adjust main.js for SoC

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,97 +1,106 @@
-// MODEL
 
+// MODEL
 // Create a prototype object for generating cats
 function Kitty(name) {
   this.name = name,
   this.clickCount = 0
 };
 
-// VIEW
-
-// Generate HTML to display cats from playPen array.
-function renderCat() {
-  for (let cat of playPen) {
-    $(`
-      <li id="${cat.name}" class="cat hidden">
-        <div  class="cat__previewbox">
-          <div class="cat__overlay"></div>
-          <p class="title">${cat.name}</p>
-          <img src="${cat.photo}" class="cat__preview" alt="photo of ${cat.name} the cat">
-          <p class="count"><span class="${cat.name}clicked">0</span> clicks</p>
-        </div>
-      </li>
-    `).appendTo('.catList__Container');
-  };
-};
-
-// Generate HTML to display cat list links
-function renderCatList() {
-  for (let cat of playPen) {
-    $(`
-      <li id="${cat.name}__Link" class="catList__Link">${cat.name}</li>
-    `).appendTo('.catList__Links');
-  };
-};
-
-// CONTROLLER
-
-// Define the cat names
-const cats = [
-  'kiki',
-  'buckaroo',
-  'lelouche',
-  'laHire',
-  'cheeky',
-  'whiskers',
-  'bulgaria',
-  'mr_Rogers',
-  'dog',
-  'bucko',
-  'marilyn',
-  'indy'
-];
-
 // Define an array in which we can place our cats
 let playPen = [];
 
-// Create Objects for each cat based on the cats array
-for (let [index ,cat] of cats.entries()) {
-  Kitty.prototype.update = function() {
-    this.name = cat;
-  };
-  cat = new Kitty(cat);
-  cat.photo = `https://loremflickr.com/200/200/kitten,sleeping/all/?lock=${Math.floor(Math.random() * Math.floor(1000))}`;
-  playPen.push(cat);
+// VIEW
+
+const view = {
+  init: function() {
+    // Create HTML snippet VIEW will utilize to render cat images.
+    function renderCat() {
+      for (let cat of playPen) {
+        $(`
+          <li id="${cat.name}" class="cat hidden">
+            <div  class="cat__previewbox">
+              <div class="cat__overlay"></div>
+              <p class="title">${cat.name}</p>
+              <img src="${cat.photo}" class="cat__preview" alt="photo of ${cat.name} the cat">
+              <p class="count"><span class="${cat.name}clicked">0</span> clicks</p>
+            </div>
+          </li>
+        `).appendTo('.catList__Container');
+      };
+    };
+
+    // Create HTML snippet VIEW will utilize to render cat buttons
+    function renderCatList() {
+      for (let cat of playPen) {
+        $(`
+          <li id="${cat.name}__Link" class="catList__Link">${cat.name}</li>
+        `).appendTo('.catList__Links');
+      };
+    };
+    renderCat();
+    renderCatList();
+  },
 };
 
-// Call function to render cats
-renderCatList();
-renderCat();
+// CONTROLLER
+const controller = {
+  init: function() {
 
+          // Define the cat names
+          const cats = [
+            'kiki',
+            'buckaroo',
+            'lelouche',
+            'laHire',
+            'cheeky',
+            'whiskers',
+            'bulgaria',
+            'mr_Rogers',
+            'dog',
+            'bucko',
+            'marilyn',
+            'indy'
+          ];
 
-// Add click event listener to cat images
-playPen.forEach(function(cat) {
-  $(`#${cat.name}`).click(function() {
+          // Create Objects for each cat based on the cats array
+          for (let [index ,cat] of cats.entries()) {
+            Kitty.prototype.update = function() {
+              this.name = cat;
+            };
+            cat = new Kitty(cat);
+            cat.photo = `https://loremflickr.com/200/200/kitten,sleeping/all/?lock=${Math.floor(Math.random() * Math.floor(1000))}`;
+            playPen.push(cat);
+          };
 
-    // increment count when clicked
-    cat.clickCount++;
-    $(`.${cat.name}clicked`).text(cat.clickCount);
-  });
-});
+          // Call functions to render cats and their respective buttons
+          view.init();
 
-// Add click event listener to cat list links
-playPen.forEach(function(cat) {
-  $(`#${cat.name}__Link`).click(function() {
+          // Add click event listener to cat images
+          playPen.forEach(function(cat) {
+            $(`#${cat.name}`).click(function() {
 
-    // toggle visibility on all other cats
-    playPen.forEach(function(cat) {
-      if(!$(`#${cat.name}`).hasClass('hidden')){
-        $(`#${cat.name}`).addClass('hidden');
-      };
-    });
+              // increment count when clicked
+              cat.clickCount++;
+              $(`.${cat.name}clicked`).text(cat.clickCount);
+            });
+          });
 
-    // make the clicked cat visible
-    $(`#${cat.name}`).toggleClass('hidden');
-  });
-});
+          // Add click event listener to cat list links
+          playPen.forEach(function(cat) {
+            $(`#${cat.name}__Link`).click(function() {
+
+              // toggle visibility on all other cats
+              playPen.forEach(function(cat) {
+                if(!$(`#${cat.name}`).hasClass('hidden')){
+                  $(`#${cat.name}`).addClass('hidden');
+                };
+              });
+
+              // make the clicked cat visible
+              $(`#${cat.name}`).toggleClass('hidden');
+            });
+          });
+  },
+}
+controller.init();
 
